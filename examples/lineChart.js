badb311866ea1e1b38fc15436285311ccab714c3
Jonathan Birkholz <rookieone@gmail.com>
2012-09-06 16:49:56 -0500

    bar and line combo

--- examples/lineChart.js
+++ examples/lineChart.js
@@ -8,24 +8,34 @@ lineChart= {
         { date:Date.parse("5/26/2012"), hours:3 },
         { date:Date.parse("5/30/2012"), hours:6 }
     ],
+    moneyData: [
+        { date:Date.parse("4/6/2012"),  revenue:310 },
+        { date:Date.parse("4/16/2012"), revenue:601 },
+        { date:Date.parse("4/26/2012"), revenue:102 },
+        { date:Date.parse("5/6/2012"),  revenue:357 },
+        { date:Date.parse("5/26/2012"), revenue:356 },
+        { date:Date.parse("5/30/2012"), revenue:890 }
+    ],    
     width: 500,
     height: 200,
     margin: 50,
+    barWidth: 20,
     svgGraph: {},
     scaleX: function(){},
     scaleY: function(){},
+    moneyScaleY: function(){},
     createSvgGraph: function(divId) {
      lineChart.svgGraph = d3.select(divId)
         .append("svg:svg")
         .attr("class", "lineChart")
-        .attr("width", lineChart.width + lineChart.margin)
+        .attr("width", lineChart.width + lineChart.margin*2)
         .attr("height", lineChart.height + lineChart.margin)
         .append("svg:g");
     },
     createScales: function() {
         var startDate = Date.parse("4/1/2012");
-        var endDate = Date.parse("5/30/2012");
-        lineChart.scaleX = d3.time.scale().domain([startDate, endDate]).range([lineChart.margin, lineChart.width]);
+        var endDate = Date.parse("6/3/2012");
+        lineChart.scaleX = d3.time.scale().domain([startDate, endDate]).range([lineChart.margin, lineChart.width-lineChart.margin]);
         lineChart.scaleY = d3.scale.linear().domain([0, 6]).range([lineChart.height, lineChart.margin]);
     },
     addLine: function() {
@@ -83,6 +93,47 @@ lineChart= {
                     $("#tool-tip").html();
                 });
     },
+    createMoneyScale: function() {
+        var revenues = lineChart.moneyData.map(function(d) { return d.revenue; });
+        var max = d3.max(revenues);
+        console.log(max);
+        lineChart.moneyScaleY = d3.scale.linear().domain([0, max]).range([lineChart.height, lineChart.margin]);
+        console.log(lineChart.moneyScaleY(890));
+    },
+    drawMoneyBars: function() {
+
+        lineChart.svgGraph.selectAll(".money-bars")
+            .data(lineChart.moneyData)
+            .enter()
+            .append("svg:rect")
+            .attr("x", function(d) {
+                return lineChart.scaleX(d.date) - (lineChart.barWidth/2);
+            })
+            .attr("y", function(d) {
+                return lineChart.moneyScaleY(d.revenue);
+            })
+            .attr("height", function(d) {
+                return lineChart.height - lineChart.moneyScaleY(d.revenue);
+            })
+            .attr("width", lineChart.barWidth)
+            .attr("fill", "#049cbd")
+            .attr("opacity", 0.5);
+    },
+    drawMoneyAxis: function() {
+        var moneyAxis = d3.svg.axis()
+                .orient("right")
+                .ticks(6)
+                .tickSize(6,1)
+                .scale(lineChart.moneyScaleY)
+                .tickFormat(function(d) {
+                    return "$" + d3.format(",.0f")(d);
+                });
+
+        lineChart.svgGraph.append("svg:g")
+                .attr("class", "y-axis")
+                .attr("transform", "translate(" + (lineChart.width - lineChart.margin) + ",0)")
+                .call(moneyAxis);
+    },
     setupSlides: function() {
         // Add Line
         lineChart.createSvgGraph("#lineChart-addLine");
@@ -109,6 +160,29 @@ lineChart= {
         lineChart.addXaxis();
         lineChart.addYaxis();  
         lineChart.addPoints();  
+
+        // Money Axis
+        lineChart.createSvgGraph("#lineChart-moneyAxis");
+        lineChart.createScales();
+        lineChart.addLine();
+        lineChart.addXaxis();
+        lineChart.addYaxis();  
+        lineChart.addPoints(); 
+        lineChart.createMoneyScale();
+        lineChart.drawMoneyAxis();
+        
+        // Money Bars
+        lineChart.createSvgGraph("#lineChart-moneyBars");
+        lineChart.createScales();
+        
+        lineChart.addXaxis();
+        lineChart.addYaxis();  
+        
+        lineChart.createMoneyScale();
+        lineChart.drawMoneyAxis();        
+        lineChart.drawMoneyBars();
+        lineChart.addPoints(); 
+        lineChart.addLine();
     }
 
 }
